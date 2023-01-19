36a2fd2b0f9179e5d68b084968d868ff9ce255ef
anthonyy2711 <amreyesnarvaez@gmail.com>
2023-01-19 13:04:23 +0100

    asd

--- src/app/class/user.ts
+++ src/app/class/user.ts
@@ -1,13 +1,45 @@
 export class User {
-   
 
+    #user_name: String;
+    #password:String;
+    email:String;
+    marital_satus:String;
+    gender:String;
+    information:String;
+    role:String;
+
+   
     constructor(
-        public user:String,
-        public password:String,
-        public email:String,
-        public marital_satus:String,
-        public gender:String,
-        public information:String,
-        public role:String,
-    ){}
+        user_name: String,
+        password:String,
+        email:String,
+        marital_satus:String,
+        gender:String,
+        information:String,
+        role:String,
+    ){
+       this.#user_name = user_name;
+       this.#password = password;
+       this.email = email;
+       this.marital_satus = marital_satus;
+       this.gender = gender;
+       this.information = information;
+       this.role = role;
+       
+    }
+
+    get user_name(): string {
+        return this.user_name;
+    }
+    set user_name(value: string) {
+        if (value == '') {
+            throw new Error('');
+        }
+        this.user_name = value;
+    }
+
+
+
+    
+
 }
