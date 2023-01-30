1014f8dd0390851621777798eaa33b5cf4dd0d9e
Ruyou Hu Ye <ruyouhuye@pop-os.localdomain>
2023-01-30 12:16:32 +0100

    borrado de comentarios en el service-event.ts

--- src/app/services/service-event.service.ts
+++ src/app/services/service-event.service.ts
@@ -10,16 +10,6 @@ export class ServiceEventService {
 
 
   }
-  /*
-  //creacion de eventos manual
-  event1 = new Event('Duke Times', 'Concierto', 2023-06-15T00:00:00,'Barcelona',100);
-  event2 = new Event('Eventions Parade Festivities', 'Cine', '2023-05-12T00:00:00','Madrid',130);
-  event3 = new Event('Proteus Live Entertainment', 'Museo', '2023-07-18T00:00:00','Valencia',170);
-  event4 = new Event('Pearl Party Division', 'Fira', '2023-08-13T00:00:00','Murcia',1430);
-  
-  //añadir los eventos manueales al array events
-  events = [this.event1,this.event2,this.event3,this.event4];
-  */
   events:Event[] = [];
   //Informacion para generar randoms
   eventNames = [
